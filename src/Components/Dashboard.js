8d5b44450a776316fc7d8bc13c1043901afee68f
Praharsh Parihar <67071977+shorya258@users.noreply.github.com>
2024-07-21 03:44:53 +0530

    initial card data render

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Dashboard() {
+  const [orders, setOrders] = useState([]);
   const fetchOrders = async () => {
     let url = "https://test-api.achilyon.in/v1/orders/all-orders";
     let options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-         "Authorization": "Bearer "+localStorage.getItem("authToken"),
+        Authorization: "Bearer " + localStorage.getItem("authToken"),
       },
       body: JSON.stringify({
         status: "PENDING",
@@ -18,13 +19,31 @@ function Dashboard() {
     let json = await response.json();
     console.log(json);
     console.log(json.data);
-    return json.data;
+    setOrders(json.data);
   };
   useEffect(() => {
     fetchOrders();
   }, []);
 
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <div className="flex gap-2 flex-wrap ">
+        {orders.map((order) => {
+          return (
+            <div className=" border-gray-500 rounded-md  border-2 border-solid flex flex-col p-4 ">
+              <h2>{order.created_by.name}</h2>
+              <p>Cancelled : {order.is_cash ? "YES" : "NO"}</p>
+              <p>Cash : {order.is_cash ? "YES" : "NO"}</p>
+              <p>Created : {order.created_at}</p>
+              <p>Status : {order.status}</p>
+              <p>Order Version : {order.order_version}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
